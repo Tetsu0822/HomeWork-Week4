8ae941e953df8cb7364a31fbcc8f84266520ee7a
Tetsu <ttgcjamg@gmail.com>
2025-07-23 13:49:12 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/scss/all.scss';
+
+console.log('Hello world');
+
+document.getElementById('favoriteBtn').addEventListener('click', function() {
+    window.location.href = 'favorite.html';
+});
